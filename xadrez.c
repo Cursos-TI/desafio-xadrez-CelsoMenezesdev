b51c4af79128526adbeb0248250e3f855132d717
CelsoMenezesdev <celso.menezesb886@gmail.com>
2026-05-23 17:56:09 -0300

    Movimentação de Peças de Xadrez - nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,84 @@
 #include <stdio.h>   
-int main() {
-    int TORRE = 1, BISPO = 1, RAINHA = 1, CAVALO = 1, ESQUERDA = 1;
-    
-    // iniciando com torre.
-
-    printf ("***Movimentos da Torre***:\n");
    
-    for (TORRE = 1; TORRE <= 5; TORRE++) {
-        printf("%d Direita, cima.\n", TORRE);
-    }
-   
-    // iniciando com bispo.
-    
-    printf ("\n***Movimentos do Bispo***:\n"); 
-    
-    while (BISPO <= 5)
-    {
-        printf("%d Direita.\n", BISPO);
-        BISPO++;
-    }
-    
-    // iniciando com rainha.    
- 
-    printf ("\n***Movimentos da Rainha***:\n");
- 
-    do
-  {
-        printf("%d Esquerda.\n", RAINHA);
-        RAINHA++;
-  } while (RAINHA <= 8);
- 
-  // Fazendo o movimento do cavalo
- 
-  printf ("\n***Movimentos do Cavalo***:\n");
+//movimento da torre
+    void moverTorre(int TORRE) {
 
-  for (CAVALO = 1; CAVALO < 2; CAVALO++);
-{
-   printf("%d Baixo. \n", CAVALO);
-} 
- while (ESQUERDA < 1) ESQUERDA++;
-{
-    printf("%d Esquerda. \n", ESQUERDA);
+    // condição de parada
+    if (TORRE > 5) {
+        return;
+    }
+
+    printf("%d - Direita\n", TORRE);
+
+    // chamada recursiva
+    moverTorre(TORRE + 1);
 }
+  
+// movimento da rainha
+void moverRainha(int RAINHA) {
+
+    // condição de parada
+    if (RAINHA > 8) {
+        return;
+    }
+
+    printf("%d - Esquerda\n", RAINHA);
+
+    // chamada recursiva
+    moverRainha(RAINHA + 1);
+}
+  //movimento do bispo
+  void moverBispo(int BISPO) {
+    if (BISPO > 5) {
+        return;
+    }
+    for (int vertical = 1; vertical <= 1; vertical++) {
+
+        for (int horizontal = 1; horizontal <= 1; horizontal++) {
+
+            printf("%d - Cima Direita\n", BISPO);
+
+        }
+    }
+
+    // chamada recursiva
+    moverBispo(BISPO + 1);
+}
+int main() {
+
+    int TORRE = 1, BISPO = 1, RAINHA = 1, CAVALO = 1, ESQUERDA = 1;
+
+    // ===== TORRE =====
+    printf("*** Movimentos da Torre ***\n");
+
+    moverTorre(TORRE);
+
+    // ===== RAINHA =====
+    printf("\n*** Movimentos da Rainha ***\n");
+
+    moverRainha(RAINHA);
+
+    // ===== BISPO =====
+    printf("\n*** Movimentos do Bispo ***\n");
+
+    moverBispo(BISPO);
+
+    // ===== CAVALO =====
+    printf("\n*** Movimentos do Cavalo ***\n");
+
+    // 2 casas para cima e 1 para direita
+    for (CAVALO = 1, ESQUERDA = 1; CAVALO <= 2; CAVALO++) {
+
+        printf("%d - Cima\n", CAVALO);
+
+        if (CAVALO == 2) {
+
+            printf("%d - Direita\n", ESQUERDA);
+
+            // uso do break
+            break;
+        }
+    }
+
     return 0;
 }
